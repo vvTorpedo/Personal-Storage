3da26ef9c1ceb264f07b6bbf200c844abe6f7589
vvTorpedo <rifflefan19@gmail.com>
2022-01-14 16:32:40 -0600

    l

--- app.js
+++ app.js
@@ -8,11 +8,9 @@ app.set('view engine', 'pug')
 
 app.use(cookies.express('a', 'b', 'c'))
 
-app.use(express.static(`${__dirname}/assets`))
+app.use(express.static(`${__dirname}/img`))
 
 app.use('/', rootRoutes)
-
-app.get('*', (req, res) => res.render('errors/404'))
 
 const port = 4444;
 app.listen(port, () => console.log(`Server Is Live On Port ${port}`))
